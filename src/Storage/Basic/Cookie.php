75d4c22733202aad8ff80e5fe5f893e2bdfcc3ca
Christopher Kalkhoff <krzysztof.kalkhoff@gmail.com>
2017-12-17 17:20:12 +0100

    Added cookie specific functions support

--- src/Storage/Basic/Cookie.php
+++ src/Storage/Basic/Cookie.php
@@ -8,6 +8,8 @@
 namespace Minwork\Storage\Basic;
 
 use Minwork\Storage\Object\AbstractArrayStorage;
+use Minwork\Storage\Interfaces\StorageInterface;
+use Minwork\Helper\Formatter;
 
 /**
  * Basic implementation of $_COOKIE storage
@@ -21,5 +23,19 @@ class Cookie extends AbstractArrayStorage
     public function __construct()
     {
         parent::__construct($_COOKIE);
+    }
+    
+    public function set($key, $value): StorageInterface
+    {
+        $args = array_slice(func_get_args(), 2);
+        setcookie(Formatter::toString($key, false), $value, ...$args);
+        
+        return $this;
+    }
+    
+    public function unset($key): StorageInterface
+    {
+        setcookie(Formatter::toString($key, false), $value, time() - 3600);
+        return $this;
     }
 }
