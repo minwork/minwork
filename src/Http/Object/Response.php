9926d0fd32b333e1346c46f445f2753e39c8d5ce
Christopher Kalkhoff <krzysztof.kalkhoff@gmail.com>
2019-03-13 23:49:28 +0100

    Changed default Response content type

--- src/Http/Object/Response.php
+++ src/Http/Object/Response.php
@@ -61,7 +61,7 @@ class Response implements ResponseInterface
      * @param string $contentType            
      * @param int $httpCode            
      */
-    public function __construct($content = null, string $contentType = self::CONTENT_TYPE_TEXT, int $httpCode = HttpCode::OK)
+    public function __construct($content = null, string $contentType = self::CONTENT_TYPE_HTML, int $httpCode = HttpCode::OK)
     {
         $this->setContent($content)
             ->setContentType($contentType)
