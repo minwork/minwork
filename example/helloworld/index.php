109e6ebcdc5e5ca7e029cd9008f48bd4c73dfe29
minwork <20630104+minwork@users.noreply.github.com>
2019-08-15 23:21:47 +0200

    Swapped hello world example autoloader to composer

--- example/helloworld/index.php
+++ example/helloworld/index.php
@@ -1,6 +1,8 @@
 <?php
-require '../../src/Core/Autoloader.php';
-MinworkAutoloader::registerDefault();
+/*require '../../src/Core/Autoloader.php';
+MinworkAutoloader::registerDefault();*/
+
+require '../../vendor/autoload.php';
 
 use Minwork\Core\Framework;
 use Minwork\Http\Utility\Environment;
@@ -8,11 +10,10 @@ use Minwork\Http\Object\Router;
 use Minwork\Basic\Controller\Controller;
 
 $controller = new class() extends Controller {
-
     public function show($name)
     {
         return "Hello {$name}!";
     }
 };
-$framework = new Framework(new Router(['default_controller' => $controller]), new Environment());
+$framework = new Framework(new Router([Router::DEFAULT_CONTROLLER_ROUTE_NAME => $controller]), new Environment());
 echo $framework->run('/World', true)->getContent();
