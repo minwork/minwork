160e8c4e6f6075ba413f6696f4da658562890fc8
minwork <20630104+minwork@users.noreply.github.com>
2019-08-20 14:49:58 +0200

    Added Condition class alias for more compact condition building

---
+++ src/Database/Utility/Cond.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Minwork\Database\Utility;
+
+/**
+ * Class Cond
+ * Alias for Condition class for even more compact conditions building
+ * @package Minwork\Database\Utility
+ * @see \Minwork\Database\Utility\Condition
+ */
+class Cond extends Condition
+{
+
+}
